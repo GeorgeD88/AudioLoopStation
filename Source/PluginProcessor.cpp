8c47f36c175e98ad4038054edd852fbb303804c3
George Doujaiji <GeorgeD.88888@gmail.com>
2026-05-13 19:08:04 -0400

    Tests: integration tests for Tasks 17/18/23/41 + test-build enablement

--- Source/PluginProcessor.cpp
+++ Source/PluginProcessor.cpp
@@ -437,7 +437,11 @@ bool AudioLoopStationAudioProcessor::hasEditor() const
 
 juce::AudioProcessorEditor* AudioLoopStationAudioProcessor::createEditor()
 {
+#ifdef ALS_TESTING
+    return nullptr;
+#else
     return new AudioLoopStationEditor (*this);
+#endif
 }
 
 //==============================================================================
